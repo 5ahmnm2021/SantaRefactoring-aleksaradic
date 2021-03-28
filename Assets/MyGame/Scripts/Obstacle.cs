dfb7165d7ae9aa70765f5f197e6681d70f7a9c45
aleksaradic <aleksaradic01@gmail.com>
2021-03-28 17:29:05 +0200

    Cleaned up the spacing

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -1,45 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-Rigidbody2D rb;
-[SerializeField] private float MoveSpeed;
+    Rigidbody2D rb;
+    [SerializeField] private float MoveSpeed;
 
-private void Awake()
-{
-    rb = GetComponent<Rigidbody2D>();
-}
-
-// Start is called before the first frame update
-void Start()
-{
-        
-}
-
-// Update is called once per frame
-void Update()
-{
-    //if obstacle's position x is < -15f it will be destroyed
-    if(transform.position.x < -15f)
+    private void Awake()
     {
-        Destroy(gameObject);
-}
-        //if obstacle's position x is < -15f it will be destroyed
-        if (transform.position.x > 15f)
-        {
-            Destroy(gameObject);
-        }
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if obstacle's position x is < -15f it will be destroyed
+        if(transform.position.x < -15f)
+        {
+            Destroy(gameObject);
+    }
+            //if obstacle's position x is < -15f it will be destroyed
+            if (transform.position.x > 15f)
+            {
+                Destroy(gameObject);
+            }
+
+        }
 
 
-private void FixedUpdate()
-{
 
-    rb.velocity = Vector2.left * MoveSpeed;
+    private void FixedUpdate()
+    {
 
-}
+        rb.velocity = Vector2.left * MoveSpeed;
+
+    }
 }
